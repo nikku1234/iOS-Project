9e90489aea7430aaefd32938faec126ab0e4de89
Nikhil Ramesh <nikhilramesh1234@gmail.com>
2018-12-05 11:53:35 +0530

    Add files via upload

---
+++ Today/Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nikhil Ramesh on 10/06/18.
+//  Copyright © 2018 Nikhil Ramesh. All rights reserved.
+//
+
+import Foundation
+
+
+class Item
+{
+    var title: String = ""
+    var done: Bool = false
+}
