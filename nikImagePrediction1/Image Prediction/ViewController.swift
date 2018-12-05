ec93049eda9ad81f946814f000c0ce32301844e3
Nikhil Ramesh <nikhilramesh1234@gmail.com>
2018-12-05 12:01:40 +0530

    Add files via upload

---
+++ nikImagePrediction1/Image Prediction/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Image Prediction
+//
+//  Created by Nikhil Ramesh on 15/06/18.
+//  Copyright © 2018 Nikhil Ramesh. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            
+             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("could not convert")
+            }
+            detect(image: ciimage)
+        }
+        
+       imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    func detect(image: CIImage)
+    {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading COREML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model){(request,error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("model fail to process image")
+            }
+            
+            if let firstResult = results.first{
+                self.navigationItem.title = firstResult.identifier
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+        try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
+        
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+
+
+}
+
